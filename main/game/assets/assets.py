0bd4b20ee8d39e8a85f19fc4d4465023fee25647
paul-talma <109061871+paul-talma@users.noreply.github.com>
2025-03-04 13:19:16 -0800

    add typing, board update functions

--- main/game/assets/assets.py
+++ main/game/assets/assets.py
@@ -1,5 +1,8 @@
 import json
+import utils.utils as utils
 
+type Dir = tuple[int, int]
+type Pos = tuple[int, int]
 
 # directions
 UP = (-1, 0)
@@ -24,15 +27,19 @@ KEY = "KEY"
 
 class Object:
     def __init__(self):
-        self.pushable: bool
-        self.x: int
-        self.y: int
+        self.pos: Pos
+        self.is_push: bool
+        self.is_stop: bool
+        self.is_win: bool
+        self.is_loss: bool
 
 
 class Text(Object):
     def __init__(self, text: str):
-        self.pushable = True
-        self.text = text
+        self.pushable: bool = True
+        self.text: str = text
+        self.is_win = False
+        self.is_loss = False
 
 
 class Wall(Object):
@@ -68,10 +75,17 @@ class Rule(AST):
 
 class Board:
     def __init__(self, path):
-        self.board = self._load_board(path)
+        self.board: dict[int, dict[int, list[Object | None]]] = self._load_board(path)
         self.rules = self._get_rules()
+        self.objects: list[Object] = []
+        self.win_objects: list[Object] = []
+        self.loss_objects: list[Object] = []
+        self.you_pos: Pos
+        self.you_objects: list[Object]
+        self.n_rows: int = len(self.board)
+        self.n_cols: int = len(self.board[0])
 
-    def _load_board(self, path):
+    def _load_board(self, path: str):
         """
             reads the initial state of the board from a .json file
             and stores it in a dictionary.
@@ -95,11 +109,17 @@ class Board:
 
         return board
 
-    def _has_text(self, pos):
+    def _get_objects(self, pos: Pos):
+        """
+            returns a list of all objects at a board pos
+        """
+        return self.board[pos[0]][pos[1]]
+
+    def _has_text(self, pos: Pos):
         """
             checks whether board[pos] contains a text block
         """
-        return any([isinstance(ob, Text) for ob in self.board[pos[0]][pos[1]]])
+        return any([isinstance(ob, Text) for ob in self._get_objects(pos)])
 
     def _get_rules(self):
         """
@@ -119,7 +139,7 @@ class Board:
                         new_rules = [self._parse_rule(rule_text) for rule_text in rule_texts]
                         rules.extend(new_rules)
 
-    def _get_text_blocks(self, pos, dir):
+    def _get_text_blocks(self, pos: Pos, dir: Dir):
         """
             scans along dir for text blocks
 
@@ -131,25 +151,19 @@ class Board:
                 text: list of text strings encountered
         """
         # TODO: handle multiple text objects in same block
-        text = []
+        text_blocks = []
         while self._in_bounds(pos) and self._has_text(pos):
-            for ob in self.board[pos[0]][pos[1]]:
+            for ob in self._get_objects(pos):
                 if isinstance(ob, Text):
-                    text.append(ob.text)
-            pos = self._vec_add(pos, dir)
-        return text
+                    text_blocks.append(ob.text)
+            pos = utils.vec_add(pos, dir)
+        return text_blocks
 
-    def _in_bounds(self, pos):
+    def _in_bounds(self, pos: Pos):
         """
             returns True iff pos is in the bounds of the board
         """
         return 0 < pos[0] < self.n_rows and 0 < pos[1] < self.n_cols
-
-    def _vec_add(self, v1, v2):
-        """
-            performs vector addition
-        """
-        return (v1[0] + v2[0], v1[1] + v2[1])
 
     def _parse_rule(self, text_blocks):
         """
@@ -191,9 +205,54 @@ class Board:
                 tree: a syntax tree representing the structure of the text, or
                     None if the text does not correspond to a valid rule
         """
-        rules = []
+        pass
 
+    def _update_board(self, dir: Dir):
+        """
+            updates the position of each YOU object
+        """
+        for you in self.you_objects:
+            if self._can_move(you, dir):
+                self._update_posisition(you, dir)
 
+    def _can_move(self, object: Object, dir: Dir):
+        """
+            returns true if object can move in direction dir
+        """
+        curr_pos = object.pos
+        next_pos = utils.vec_add(curr_pos, dir)
+
+        if not self._in_bounds(next_pos):
+            return False
+
+        for obj in self._get_objects(next_pos):
+            if obj.is_stop:
+                return False
+
+            if obj.is_push and not self._can_move(obj, dir):
+                return False
+
+        return True
+
+    def _update_posisition(self, curr_pos: Pos, dir: Dir):
+        next_pos = utils.vec_add(curr_pos, dir)
+        next_objects = self._get_objects(next_pos)
+
+        if all([not o.is_push for o in next_objects]):
+            self._move_tile(curr_pos, next_pos)
+            return
+
+        self._update_posisition(next_pos, dir)
+        self._move_tile(curr_pos, next_pos)
+
+    # TODO: Pick up here
+    def _move_tile(self, curr_pos: Pos, dir: Dir):
+        curr_objects = self._get_objects(curr_pos)
+
+        you_indices = []
+        for id, object in enumerate(curr_objects):
+            if object.is_you:
+                pass
 
     def update(self, input):
         pass
