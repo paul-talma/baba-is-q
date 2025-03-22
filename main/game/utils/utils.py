6091e07d3cadc93e2ddc7eaa0123cf3f84895caa
paul-talma <109061871+paul-talma@users.noreply.github.com>
2025-03-22 16:40:06 -0700

    update type system

--- main/game/utils/utils.py
+++ main/game/utils/utils.py
@@ -1,9 +1,22 @@
-def vec_add(self, v1, v2):
+def vec_add(v1: tuple[int, int], v2: tuple[int, int]):
     """
-        performs vector addition
+    performs vector addition
     """
     return (v1[0] + v2[0], v1[1] + v2[1])
 
+
+#############
+#   TYPES   #
+#############
+OBJECT = "OBJECT"
+VERB = "VERB"
+ACTION = "ACTION"
+CONNECTIVE = "CONNECTIVE"
+
+
+#############
+#   TEXT    #
+#############
 
 # objects
 WALL = "WALL"
@@ -26,36 +39,46 @@ YOU = "YOU"
 PUSH = "PUSH"
 WIN = "WIN"
 
-# misc
+# connectives
 AND = "AND"
+
+
+class Token:
+    def __init__(self, type: str, text: str) -> None:
+        self.type = type
+        self.text = text
 
 
 class AST:
     pass
 
 
-class SyntaxObject(AST):
-    def __init__(self, object_type):
-        pass
+class TextObject(AST):
+    pass
 
 
-class Verb(AST):
-    def __init__(self, text: str):
-        self.text = text
+class Object(TextObject):
+    def __init__(self, name) -> None:
+        self.name = name
 
 
-class Action(AST):
-    def __init__(self, text: str):
-        self.text = text
+class Verb(TextObject):
+    def __init__(self, verb: str) -> None:
+        self.verb = verb
+
+
+class Action(TextObject):
+    def __init__(self, action: str) -> None:
+        self.action = action
 
 
 class ComplexObject(AST):
-    def __init__(self, object_list: list[SyntaxObject]):
+    def __init__(self, object_list: list[TextObject]) -> None:
         self.object_list = object_list
 
 
 class Rule(AST):
-    def __init__(self, complex_object: SyntaxObject, verb: Verb, obj: SyntaxObject):
+    def __init__(self, complex_object: TextObject, verb: Verb, obj: TextObject) -> None:
         self.complex_object = complex_object
         self.verb = verb
         self.obj = obj
@@ -63,79 +86,94 @@ class Rule(AST):
 
 class Parser:
     """
-        parses a text block into a rule
+    parses a text block into a rule
 
-        the grammar of the language is specified by the following BNF:
+    the grammar of the language is specified by the following BNF:
 
-        RULE: complex_object verb object
-            | complex_object IS action
+    RULE: complex_object verb object
+        | complex_object IS action
 
-        object: BABA
-              | KEKE
-              | WALL
-              | FLAG
-              | ROCK
-              | WATER
-              | KEY
-              | DOOR
+    object: BABA
+          | KEKE
+          | WALL
+          | FLAG
+          | ROCK
+          | WATER
+          | KEY
+          | DOOR
 
-        verb: IS
-            | HAS
-            | MAKE
+    verb: IS
+        | HAS
+        | MAKE
 
-        action: PUSH
-              | STOP
-              | YOU
+    action: PUSH
+          | STOP
+          | YOU
 
-        complex_object: object (AND object)*
+    complex_object: object (AND object)*
 
 
-        note that the grammar for complex objects has been simplified to
-            exclude adjectives or modifiers
+    note that the grammar for complex objects has been simplified to
+        exclude adjectives or modifiers
 
-        params:
-            text_blocks: a list of strings corresponding to the text
-                on contiguous text blocks
+    params:
+        text_blocks: a list of strings corresponding to the text
+            on contiguous text blocks
 
-        returns:
-            tree: a syntax tree representing the structure of the text, or
-                None if the text does not correspond to a valid rule
+    returns:
+            None if the text does not correspond to a valid rule
     """
 
-    def __init__(self, text: list[str] = None):
-        self.text: list[str] = text
-        self.token: str = text[0]
+    def __init__(self, tokens: list[Token]):
+        """
+        params:
+            tokens: list of Token objects
+        """
+        self.tokens = tokens
+        self.token: Token = tokens[0]
         self.pos: int = 0
-        self.len: int = len(text)
+        self.len: int = len(tokens)
 
+    # TODO: does this need a param?
     def _parse(self, text: list[str]):
         self._rule()
 
-    def _eat(self, token: str):
-        if self.token == token:
-            self.token = self._next_token()
+    # TODO: graceful exit
+    def _exit(self):
+        pass
+
+    def _eat(self, type: str):
+        if self.token.type == type:
+            tk = self._next_token()
+            if tk:
+                self.token = tk
+            else:
+                self._exit()
         else:
-            raise Exception()
+            # TODO: need to fail gracefully here
+            self._exit()
 
     def _next_token(self):
         self.pos += 1
         if self.pos < self.len:
-            return self.text[self.pos]
-        return "EOF"
+            return self.tokens[self.pos]
+        return None
 
-    def _rule(self):
+    def _rule(self) -> Rule:
         """
-            RULE: complex_object verb object
-                | complex_object IS action
+        RULE: complex_object verb object
+            | complex_object IS action
         """
         complex_object = self._complex_object()
         verb = self._verb()
         obj = self._obj()
+        if obj.text == ACTION and verb.verb != IS:
+            self._exit()
         return Rule(complex_object, verb, obj)
 
-    def _complex_object(self):
+    def _complex_object(self) -> ComplexObject:
         """
-            complex_object: object (AND object)*
+        complex_object: object (AND object)*
         """
         object_list: list[str] = []
         object_list.append(self._obj())
@@ -146,74 +184,36 @@ class Parser:
 
         return ComplexObject(object_list)
 
-    def _verb(self):
+    def _verb(self) -> Verb:
         """
-            verb: IS
-                | HAS
-                | MAKE
+        verb: IS
+            | HAS
+            | MAKE
         """
-        token = self.token
-        if token == IS:
-            self._eat(token)
-            return Verb(text=IS)
-        if token == HAS:
-            self._eat(token)
-            return Verb(text=HAS)
-        if token == MAKE:
-            self._eat(token)
-            return Verb(text=MAKE)
+        tk = self.token
+        self._eat(tk.type)
+        return Verb(tk.text)
 
-    def _obj(self):
+    def _obj(self) -> Object:
         """
-            object: BABA
-                  | KEKE
-                  | WALL
-                  | FLAG
-                  | ROCK
-                  | WATER
-                  | KEY
-                  | DOOR
+        object: BABA
+              | KEKE
+              | WALL
+              | FLAG
+              | ROCK
+              | WATER
+              | KEY
+              | DOOR
         """
-        token = self.token
-        self._eat(token)
-        return SyntaxObject(token)
-        # if token == BABA:
-        #     self._eat(token)
-        #     return SyntaxObject(BABA)
-        #
-        # if token == KEKE:
-        #     self._eat(token)
-        #     return SyntaxObject(KEKE)
-        #
-        # if token == WALL:
-        #     self._eat(token)
-        #     return SyntaxObject(WALL)
-        #
-        # if token == FLAG:
-        #     self._eat(token)
-        #     return SyntaxObject(FLAG)
-        #
-        # if token == ROCK:
-        #     self._eat(token)
-        #     return SyntaxObject(ROCK)
-        #
-        # if token == WATER:
-        #     self._eat(token)
-        #     return SyntaxObject(WATER)
-        #
-        # if token == KEY:
-        #     self._eat(token)
-        #     return SyntaxObject(KEY)
-        #
-        # if token == DOOR:
-        #     self._eat(token)
-        #     return SyntaxObject(token)
+        tk = self.token
+        obj = Object(tk.text)
+        self._eat(OBJECT)
+        return obj
 
-    def _action(self):
-        token = self.token
-        self._eat(token)
-        return Action(token)
-
+    def _action(self) -> Action:
+        tk = self.token
+        self._eat(tk.type)
+        return Action(tk.text)
 
 
 # want the parser to be created once, then have a method to feed it rule texts and parse these
