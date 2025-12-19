c0a369ae75a79e3b3acc4c55e457630198c7ed36
paul-talma <109061871+paul-talma@users.noreply.github.com>
2025-12-19 23:58:36 +0100

    board rendering

--- main/game/utils/utils.py
+++ main/game/utils/utils.py
@@ -3,3 +3,15 @@ def vec_add(v1, v2):
     performs vector addition
     """
     return (v1[0] + v2[0], v1[1] + v2[1])
+
+
+def render_board(board):
+    for row in board.board:
+        for tile in row:
+            if len(tile) > 1:
+                print('!', end='')
+            elif tile:
+                print(tile[0], end='')
+            else:
+                print(' ', end='')
+        print('\n', end='')
